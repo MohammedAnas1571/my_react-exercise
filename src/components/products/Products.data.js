a81fa5c36ece74be8d3fa21c86e1c6f859bb9c19
Mohammed Anas <mohammedanas1571@gmail.com>
2023-12-21 20:35:12 +0530

    first commit

---
+++ src/components/products/Products.data.js
@@ -0,0 +1,44 @@
+const PhoneData = [{
+    id:1,
+    model:"Nokia",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit`,
+    price:50.99,
+    image:'smart4',
+    qty:1
+},
+{
+    id:2,
+    model:"Apple",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit`,
+    price:520.99,
+    image:'smart4',
+    qty:1
+},
+{
+    id:3,
+    model:"Oppo",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit`,
+    price:6510.99,
+    image:"banner-2",
+    qty:1
+},
+{
+    id:4,
+    model:"Huwai",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit`,
+    price:5540.99,
+    image:'banner-3',
+    qty:1
+},
+{
+    id:5,
+    model:"Samsung",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit`,
+    price:504.99,
+    image:'banner-3',
+    qty:1
+
+},
+]
+
+export default PhoneData

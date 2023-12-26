e311d96868f42e59ea5f3b22c941ab766bc53d5e
Mohammed Anas <mohammedanas1571@gmail.com>
2023-12-26 15:52:34 +0530

    26 th day commit

--- src/components/ReactRouter/Todoapp/AddTask.js
+++ src/components/ReactRouter/Todoapp/AddTask.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import { useState } from 'react'
-const Addtask = ({addTask}) => {
-  const [value,setValue]=useState("")
-  const addItem = ()=>{
-  addTask(value)
-  setValue("");
-  }
+import React, { useState } from "react";
+
+const AddTask = ({ addTask }) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+
+  const addItem = () => {
+    if (value.trim() !== "") {
+      addTask(value);
+      setValue("");
+      setError("");
+    } else {
+      setError("Please enter Something.");
+    }
+  };
 
   return (
-    <div className='input-container'>
-        <input type="text" value={value} className='input' onChange={(e)=>{setValue(e.target.value)}} placeholder='Please Add'/>
-        <button className='add-btn' onClick={addItem}>ADD</button>
+    <div className="input-container">
+      <input
+        type="text"
+        value={value}
+        className="input"
+        onChange={(e) => {
+          setValue(e.target.value);
+          setError("");
+        }}
+        placeholder="Please Add"
+      />
+      <button className="add-btn" onClick={addItem}>
+        ADD
+      </button>
+      {error && <p className="error-message">{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default Addtask
+export default AddTask;
--- src/components/ReactRouter/Todoapp/ListTask.js
+++ src/components/ReactRouter/Todoapp/ListTask.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const ListTask = ({task,index,removeTask}) => {
-   
-    
+const ListTask = ({ task, index, removeTask }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <>
-    <div className='list-task'>
-        {task.title}
-      <button className='delete-button'onClick={()=>{removeTask(index)}}>Delete</button>
+    <div className={`list-task ${isChecked ? 'checked' : ''}`}>
+      <input type='checkbox' onChange={handleCheckboxChange} checked={isChecked} />
+      <span>{task.title}</span>
+      <button className='delete-button' onClick={() => removeTask(index)}>
+        Delete
+      </button>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default ListTask
+export default ListTask;
